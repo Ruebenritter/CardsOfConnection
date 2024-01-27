6a491de97eaee97d049fea6b2b41e03d669fb6ee
thomas3913 <thomas_s_42@gmx.de>
2024-01-27 10:49:35 +0100

    added mainmenu, buttons, credits, fonts

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public SceneChanger sceneChanger;
+    public GameObject StartScreen;
+    public GameObject CreditsScreen;
+    public GameObject LoseScreen;
+    public GameObject WinScreen;
+    
+    void Start()
+    {
+        
+
+        StartScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+        LoseScreen.SetActive(false);
+        WinScreen.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void OpenCredits(){
+        StartScreen.SetActive(false);
+        CreditsScreen.SetActive(true);
+    }
+
+    public void CloseCredits(){
+        StartScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+    }
+
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
