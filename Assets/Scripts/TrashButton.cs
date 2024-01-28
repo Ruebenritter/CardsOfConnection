bcd8269320329dbfff1bd7a6250a272bea507f67
thomas3913 <thomas_s_42@gmx.de>
2024-01-28 13:03:15 +0100

    update backgrounds, addde trashtexthover

--- Assets/Scripts/TrashButton.cs
+++ Assets/Scripts/TrashButton.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
-public class TrashButton : MonoBehaviour, IPointerClickHandler
+public class TrashButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private GameManager gameManager;
-public GameObject openBin;
-public GameObject closedBin;
+    public GameObject openBin;
+    public GameObject closedBin;
+
+    public TMP_Text TrashInfoText;
+    private Color TrashInfoTextColorTransp;
+    private Color TrashInfoTextColorNormal;
 
     public AudioSource clickSound;
+
+
 
     void Awake(){
 
@@ -19,12 +26,30 @@ public GameObject closedBin;
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        TrashInfoText = TrashInfoText.GetComponent<TextMeshPro>();
+        TrashInfoText.overrideColorTags = true;
+        TrashInfoTextColorNormal = new Color (0.019f,0.080f,0.065f,0.990f);
+        TrashInfoTextColorTransp = new Color (0.919f,0.980f,0.965f,0.000f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TrashInfoText.color = TrashInfoTextColorNormal;
+        Debug.Log("TrashButton entered!");
+        //LeanTween.value(gameObject, updateValueExampleCallback, fadeoutcolor, color, 1f).setEase(LeanTweenType.easeOutElastic).setDelay(2f);
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TrashInfoText.color = TrashInfoTextColorTransp;
+        Debug.Log("TrashButton leaving!");
     }
 
     public void OnPointerClick (PointerEventData eventData) {
